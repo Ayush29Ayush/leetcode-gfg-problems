ee0ff4fe0b5df69aba8733ccef6ede2b1b2e82e9
Ayush <ayushsenapati123@gmail.com>
2021-12-03 21:44:41 +0530

    power-of-four

---
+++ power-of-four/power-of-four.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        int ans = 1;
+        
+        for(int i = 0; i<=30; i++)
+        {
+            if(ans==n)
+            {
+                return true;
+            }
+            if(ans<INT_MAX/4)
+            ans = ans * 4;
+        }
+        return false;
+    }
+};
