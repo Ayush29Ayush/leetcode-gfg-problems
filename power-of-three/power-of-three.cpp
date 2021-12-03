6c61ef547de7c1bfa3f0615fcc9004f75270bd38
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2021-12-03 19:56:56 +0530

    Time: 20 ms (43.81%), Space: 5.7 MB (99.45%) - LeetHub

---
+++ power-of-three/power-of-three.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        int ans = 1;
+        
+        for(int i = 0; i<=30; i++)
+        {
+            if(ans==n)
+            {
+                return true;
+            }
+            if(ans<INT_MAX/3)
+            ans = ans * 3;
+        }
+        return false;
+    }
+};
