a06c5b6f03176615b6bd98b3728a870f0ca13a5d
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-02-27 11:46:00 +0530

    Time: 65 ms (12.60%), Space: 25.7 MB (22.97%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -3,7 +3,7 @@ public:
     void rotate(vector<int>& nums, int k) {
       vector<int> temp(nums.size());
       
-      for(int i =0;i<nums.size();i++) {
+      for(int i=0;i<nums.size();i++) {
         temp[(i+k)%nums.size()] = nums[i];
       }
         
