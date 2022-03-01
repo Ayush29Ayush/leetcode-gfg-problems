183330501b118c3a23669235c2d86bd5c9ebd8d3
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-03-01 11:49:13 +0530

    Time: 13 ms (20.03%), Space: 8.5 MB (34.00%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/counting-bits/discuss/1808016/C%2B%2B-oror-Vectors-Only-oror-Easy-To-Understand-oror-Full-Explanation
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+		
+		// iterating fromt 0 to n
+        for(int i = 0; i<=n; i++)
+        {
+			// sum is initialised as 0
+            int sum = 0;
+            int num = i;
+			// while num not equals zero
+            while(num != 0)
+            {
+				// we have to count 1's in binary representation of i, therefore % 2
+                sum += num%2;
+                num = num/2;
+            }
+			// add sum to ans vector
+            ans.push_back(sum);
+        }
+		// return 
+        return ans;
+    }
+};
