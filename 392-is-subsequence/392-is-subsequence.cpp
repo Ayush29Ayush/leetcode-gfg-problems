59090c7068f00088cb231091c97c276d86440a42
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-03-02 09:43:45 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (90.23%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/is-subsequence/discuss/1811177/C%2B%2B-oror-DP-and-Two-pointer-oror-Both-are-100faster
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n = s.length(),m=t.length();
+        int j = 0; 
+    // For index of s (or subsequence
+ 
+    // Traverse s and t, and
+    // compare current character
+    // of s with first unmatched char
+    // of t, if matched
+    // then move ahead in s
+    for (int i = 0; i < m and j < n; i++)
+        if (s[j] == t[i])
+            j++;
+ 
+    // If all characters of s were found in t
+    return (j == n);
+    }
+};
