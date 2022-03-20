f3560ae149af644a0fe4ea5380e993fac2783f56
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-03-20 08:52:51 +0530

    Time: 257 ms (15.91%), Space: 111.7 MB (29.27%) - LeetHub

---
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/discuss/1865373/C%2B%2B-oror-2-Approaches-oror-Explained-With-Algorithm-oror-Easy-and-Simple
+
+class Solution {
+public:
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        int n = tops.size();
+        
+        // faceA for counting the occurence in tops
+		// faceB for counting occurence of numbers in bottoms
+        // same for counting when both tops and bottoms have same occurences
+        vector<int> faceA(7), faceB(7), same(7);
+        
+        for(int i = 0; i < n; ++i)
+        {
+            ++faceA[tops[i]];
+            ++faceB[bottoms[i]];
+            
+            if(tops[i] == bottoms[i])
+                ++same[tops[i]];
+        }
+        
+        int minRotation = INT_MAX;
+        
+		// all possibilities from 1 to 6
+        for(int i = 1; i<=6; ++i)
+        {
+            if(faceA[i] + faceB[i] - same[i] == n)
+                minRotation = min(minRotation , min(faceA[i],faceB[i]) - same[i]);
+        }
+        
+        return minRotation == INT_MAX ? -1 : minRotation;
+        
+    }
+};
