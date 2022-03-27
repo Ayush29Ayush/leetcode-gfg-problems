643011d77b4793b1eb7c76a49eddd102abfcb9cb
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-03-27 09:55:08 +0530

    Time: 15 ms (62.61%), Space: 11 MB (7.93%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/discuss/1886772/C%2B%2B-oror-2-Approaches-oror-Algorithm-oror-Set-oror-Heap-oror-Pair-oror-STL
+
+class Solution {
+public:
+    
+    // for calculating the soldier using binary search
+    int binary(vector<int> m)
+    {
+        int l = 0;
+        int h = m.size()-1;
+        while(l <= h)
+        {
+            int mid = l + (h-l)/2;
+            if(m[mid] == 0)
+                h = mid - 1;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+    
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        
+        // a binary heap which will maintain pair of count and index {count,index}
+        priority_queue<pair<int,int> > pq;
+        vector<int> ans;
+        for(int i = 0; i<mat.size(); ++i)
+        {
+            // calularing cnt of soldier
+            int cnt = binary(mat[i]);
+			
+			// instead  of using binary func for calculating cnt of 1
+			// we can use this stl count function
+			// int cnt = count(mat[i].begin(),mat[i].end(),1);
+			
+            pq.push(make_pair(cnt,i));
+            
+            // we need to maintain top k smallest elements
+            if(pq.size() > k)
+                pq.pop();
+        }
+        
+        // making answer vector
+        for(int i = 0; i<k; ++i)
+        {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
