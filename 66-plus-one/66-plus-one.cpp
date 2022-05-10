4993127f0902c960004330b985ae9b22f2f6a770
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-05-10 23:44:37 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (12.81%) - LeetHub

---
+++ 66-plus-one/66-plus-one.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        //int n = digits.size();
+        for(int i = digits.size()-1;i>=0;i--)
+        {
+            if(digits[i]!=9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            else
+            {
+                digits[i]=0;
+            }
+        }
+        digits.insert(digits.begin(),1);
+        
+        return digits;
+    }
+};
