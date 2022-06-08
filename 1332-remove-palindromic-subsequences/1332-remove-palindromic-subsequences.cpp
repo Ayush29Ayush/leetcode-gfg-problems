f8239c89340e9ff11863d4e742a6194d85e0043a
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-06-08 10:33:29 +0530

    Time: 7 ms (7.40%), Space: 6.2 MB (26.79%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -0,0 +1,17 @@
+//! https://leetcode.com/problems/remove-palindromic-subsequences/discuss/2124549/Easy-O(n)-Solution-in-C%2B%2B-oror-Two-Pointers
+
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        if(s.size()==0) return 0;
+        int i=0, j=s.size()-1;
+        while(i<j) {
+			//If string is not palindrome return 2
+            if(s[i]!=s[j]) return 2;
+            i++;
+            j--;
+        }
+        
+        return 1;
+    }
+};
