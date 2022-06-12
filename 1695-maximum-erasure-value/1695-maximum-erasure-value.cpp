d86b52b2fde18e10f7368befe4093ac3a9052b93
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-06-12 22:03:48 +0530

    Time: 722 ms (18.35%), Space: 127.2 MB (24.42%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,9 +1,12 @@
+// https://www.youtube.com/watch?v=7YqscdGIS6Y
+
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
         int ans = 0, current=0;
         unordered_set<int> set;
         for (int i = 0, j = 0; j < nums.size(); j++) {
+            // to shrink the sliding window
             while (set.find(nums[j]) != set.end()) {
                 set.erase(nums[i]);
                 current -= nums[i];
