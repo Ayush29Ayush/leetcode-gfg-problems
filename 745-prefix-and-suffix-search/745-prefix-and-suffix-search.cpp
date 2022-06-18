bcd222303ce30032cb6573156a62836a280f75d1
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-06-18 20:29:16 +0530

    Time: 989 ms (23.89%), Space: 149.9 MB (73.14%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -1,31 +1,39 @@
-// https://leetcode.com/problems/prefix-and-suffix-search/discuss/1185249/C%2B%2B-No-TRIE-A-HashMap-soln.
+// https://leetcode.com/problems/prefix-and-suffix-search/discuss/2165519/C%2B%2B-or-Use-Hashmap-or-Simple-solution-with-comments-or-DLC-June-Day-18
 
 class WordFilter {
-   private:
-    unordered_map<string, int> hashMap;
-
-   public:
-    WordFilter(vector<string>& words) {
+public:
+    unordered_map<string, int> mp; //stores preffix and suffix sequence in it
+    
+    WordFilter(vector<string>& words) 
+    {
         int n = words.size();
-        for (int i = 0; i < n; i++) {
-            string word = words[i];
-            int wordSize = word.size();
-            for (int j = 1; j <= wordSize; j++) {
-                string p = word.substr(0, j);
-                for (int k = 0; k < wordSize; k++) {
-                    string s = word.substr(k, wordSize);
-                    hashMap[p + "|" + s] = i + 1;
+        
+        for(int i=0; i<n; i++) //cover each word in words
+        {
+            string word = words[i]; 
+            int wordsize = word.size();
+            
+            //storing all possible sequence of prefix and suffix of a word in the hashmap
+            for(int j=1; j<=wordsize; j++) //for preffix 
+            {
+                string pre = word.substr(0,j);
+                
+                for(int k=0; k<wordsize; k++) //for suffix
+                {
+                    string suff = word.substr(k, wordsize);
+                    mp[pre + "|" + suff] = i+1; //set index i+1 for all possible sequence of a word
+                    //we set i+1 to handle i=0 because by-default map initialized with 0 
                 }
             }
         }
     }
-
-    int f(string prefix, string suffix) {
+    
+    int f(string prefix, string suffix) 
+    {
         string s = prefix + "|" + suffix;
-        return hashMap[s] - 1;
+        return mp[s]-1; //if sequence found in hashmap we return its index
     }
 };
-
 /**
  * Your WordFilter object will be instantiated and called as such:
  * WordFilter* obj = new WordFilter(words);
