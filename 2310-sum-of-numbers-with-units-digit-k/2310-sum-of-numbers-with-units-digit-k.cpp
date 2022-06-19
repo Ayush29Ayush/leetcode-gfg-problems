8ff985b9a6eac114e6bb43783daf3c8ae0d4f16a
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-06-19 12:51:54 +0530

    Time: 5 ms (66.67%), Space: 6 MB (83.33%) - LeetHub

--- 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.cpp
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.cpp
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/sum-of-numbers-with-units-digit-k/discuss/2168430/C%2B%2B-easy-approach-3-liner
+
 class Solution {
 public:
     int minimumNumbers(int num, int k) {
