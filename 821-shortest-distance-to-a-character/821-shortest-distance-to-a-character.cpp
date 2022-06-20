7b7aea9a5763d6165106d5daf301adc3cc21d6be
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-06-20 13:49:04 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (17.40%) - LeetHub

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.cpp
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.cpp
@@ -1,15 +1,18 @@
+// https://leetcode.com/problems/shortest-distance-to-a-character/discuss/2173687/C%2B%2B-solution-oror-Simple-oror-Easy-understanding
+
 class Solution {
 public:
     vector<int> shortestToChar(string s, char c) {
         vector<int> position;
         vector<int> answer;
         
+        //collect all the position of given char
         for(int i=0; i<s.size(); i++)
         {
             if(s[i]==c)
                 position.push_back(i);
         }
-        
+        //traverse and find the shortest distance(min difference) from the given char to all given positions
         for(int i=0; i<s.size(); i++)
         {
             int shortest_dist = INT_MAX;
