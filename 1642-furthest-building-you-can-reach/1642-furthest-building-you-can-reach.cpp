3e4765541f6b6e693641775707970dff665f6d3f
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-06-21 19:53:27 +0530

    Time: 249 ms (10.29%), Space: 54.1 MB (14.85%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) 
+    {
+        priority_queue<int> maxBricks; //stores bricks used in each step in decreasing order
         
-        // Priority Queue for storing the bricks used in each step in decreasing order (Max at top)
-        priority_queue<int> maxB;
-    
-        int i=0, diff =0; // i is used for storing the position and diff for storing difference.
-        for(i=0; i<heights.size()-1; i++){ // go till before the last building.
+        int i=0, height_diff=0;
+        
+        //go until all bricks and ladders are used or reach at last building
+        for(i=0; i<heights.size()-1; i++)
+        {
+            height_diff = heights[i+1]-heights[i];
             
-            //difference of the height of corresponding buildings
-            diff = heights[i+1]-heights[i];
-            
-            //If next building is equal or samaller than current then go to next building.
-            if(diff <= 0){
+            //if current building's height >= next building's height, we don't need a ladder  or bricks
+            if(height_diff<=0) 
                 continue;
-            }
-
-            bricks -= diff; //taking the bricks needed for going to next building.
-            maxB.push(diff); //adding the number of bricks used in priority queue.
             
-            // if bricks become negetive then there were not enough bricks. So add a ladder in place of the step where most bricks were used.
-            if(bricks < 0){
-                bricks += maxB.top(); //taking back bricks from that step
-                maxB.pop(); //As max bricks were removed so pop
-                ladders--; //1 ladder used
+            bricks -= height_diff; //subtract bricks used from total bricksj
+            maxBricks.push(height_diff); //add bricks used in priority_queue
+            
+            if(bricks<0) //if we don't have enough bricks we use ladder
+            {
+                bricks += maxBricks.top(); //add max bricks used in previous buildings to total bricks
+                maxBricks.pop();
+                ladders--; //and used ladder their
             }
-
-            //if ladder is negetive then the ladder was not provided to go to next building. So we can't proceed.
-            if(ladders < 0) break;
+            
+            if(ladders<0) //if all ladders are used we break so currect building is ans
+                break;
         }
-        
-        // return the present position.
         return i;
     }
 };
