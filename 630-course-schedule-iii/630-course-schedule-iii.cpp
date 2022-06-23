8696bd24b83c7ef22352c941496123c19295237c
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-06-23 12:45:19 +0530

    Time: 339 ms (58.03%), Space: 63.4 MB (33.94%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
-    int scheduleCourse(vector<vector<int>>& courses) {
-        if(courses.size() <= 0) return 0;
-        sort(courses.begin(), courses.end(), [](const vector<int>& a, vector<int>& b) {
-            return a[1] < b[1];
-        });
-        priority_queue<int> q;
-        int sum = 0;
-        for(auto i : courses) {
-            sum += i[0];
-            q.push(i[0]);
-            if(sum > i[1]) {
-                sum -= q.top();
-                q.pop();
-            }
-        }
-        return q.size();
-    }
+    static bool comp(vector<int>&a, vector<int>&b) {
+		return a[1] < b[1];
+	}
+    
+	int scheduleCourse(vector<vector<int>>& courses) {
+		int n = courses.size();
+		//Step 1
+		sort(courses.begin(), courses.end(), comp);
+		priority_queue<int>pq;
+		int sum = 0;
+		//Step 2
+		for(auto it : courses) {
+			int duration  = it[0];
+			int deadline = it[1];
+			// Step 3
+			if(sum + duration <= deadline) {
+				sum += duration;
+				pq.push(duration);
+			}
+			// Step 4
+			else if(pq.size() && pq.top() > duration){ 
+				sum += duration - pq.top();
+				pq.pop();
+				pq.push(duration);
+			}
+		}
+		// Step 5
+		return pq.size();
+	}
 };
