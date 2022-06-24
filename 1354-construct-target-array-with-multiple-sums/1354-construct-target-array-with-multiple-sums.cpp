2eb11027a0a85bb6ab52b109d6e1bb36a4a08c11
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-06-24 06:15:40 +0530

    Time: 49 ms (85.31%), Space: 28.6 MB (81.82%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -1,32 +1,43 @@
 class Solution {
 public:
     bool isPossible(vector<int>& target) {
-        priority_queue<int> q;
+        if(target.size()==1)
+        {
+            return target[0] == 1;
+        }
+        
         long sum = 0;
-        
-        for (auto num : target) {
-            q.push(num);
-            sum += num;
+        for(int i=0; i<target.size();i++)
+        {
+            sum += target[i];
         }
         
-        while (!q.empty()) {
-            int curr_max = q.top();
-            q.pop();
-            if (curr_max == 1) return true;
-
-            long rest = sum - curr_max;
+        priority_queue<int> pq(target.begin(), target.end());
+        
+        // Repeat till all elements haven't been made equal to 1
+        while(pq.top() != 1)
+        {
+            long largest = pq.top();
+            pq.pop();
+            long largest_2 = pq.top();
+            long restArrSum = sum - largest;
             
-            if (rest == 0) return false;
-            if (rest == 1) return true;
-            if(rest >= curr_max) return false;
-
-            rest = curr_max % rest;
-            if (rest == 0) return false;
+            // n = number of subtraction to get largest below 2nd largest element.
+            // max(1,..) denotes the case where largest and largest_2 are equal
+            int n = max(1l, (largest-largest_2)/restArrSum);
             
-            sum -= (curr_max - rest);
-            q.push(rest);
-
+            // subtracting from largest n times
+            largest -= (restArrSum * n);
+            
+            // push element back and update the sum of array
+            pq.push(largest);
+            sum = restArrSum + largest;
+            
+            // if an element gets below 1, we cannot achieve original array
+            if(largest<1)
+                return false;
         }
+        
         return true;
     }
 };
