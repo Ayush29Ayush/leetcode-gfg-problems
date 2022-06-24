75ceab0e480a48ec4be5ec9ba11d8d364f5c17c0
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-06-24 22:54:30 +0530

    Time: 118 ms (91.66%), Space: 67.7 MB (53.54%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -4,16 +4,28 @@ public:
         int sum = 0;
         int maximum = INT_MIN;
         
-        for(int i=0; i<nums.size();i++)
-        {
-            sum += nums[i];
+//         for(int i=0; i<nums.size();i++)
+//         {
+//             sum += nums[i];
             
-            if(maximum<sum)
-                maximum = sum;
+//             if(maximum<sum)
+//                 maximum = sum;
+            
+//             if(sum<0)
+//                 sum=0;
+//         }
+        
+        // Using iterator for traversal
+        for(auto i : nums)
+        {
+            sum += i;
+            
+            maximum = max(maximum,sum);
             
             if(sum<0)
-                sum=0;
+                sum = 0;
         }
+        
         return maximum;
     }
 };
