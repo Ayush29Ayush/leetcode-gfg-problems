986d5bdf68f88578a74b580b7c550dd14ca92259
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-06-26 20:42:44 +0530

    Time: 3 ms (62.22%), Space: 7 MB (73.13%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        
         int n = matrix.size();
         
-        //taking transpose of a matrix
-        for(int i = 0; i<n; i++){
-            for(int j = i+1; j<n; j++){
-                
-                swap(matrix[i][j], matrix[j][i]);
+        // Step1 -> transpose the matrix
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
             }
         }
         
-        //need to swap 1st and last colms element to get the desired ans
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<n/2; j++){
-                
-                swap(matrix[i][j], matrix[i][n-j-1]);
-            }
+        // Step2 -> reverse the row
+        for(int i=0;i<n;i++)
+        {
+            reverse(matrix[i].begin(),matrix[i].end());
         }
-        
     }
 };
