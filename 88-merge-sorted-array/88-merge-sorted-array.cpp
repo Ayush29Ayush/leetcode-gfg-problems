13e794ff6cc96c862f8bc49d53d4dac7b1001d79
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-06-28 19:11:32 +0530

    Time: 7 ms (26.77%), Space: 9 MB (89.53%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,27 +1,8 @@
-class Solution
-{
+class Solution {
 public:
-    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
-    {
-        vector<int> nums(m + n, 0);
-        int k = 0, i = 0, j = 0;
-        if (n == 0)
-            return;
-        while (i < m && j < n)
-        {
-
-            if (nums1[i] < nums2[j])
-                nums[k++] = nums1[i++];
-
-            else
-                nums[k++] = nums2[j++];
-        }
-
-        while (i < m)
-            nums[k++] = nums1[i++];
-        while (j < n)
-            nums[k++] = nums2[j++];
-
-        nums1 = nums;
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        nums1.resize(m); // removes the additional zeroes at end of num1
+        nums1.insert(nums1.end(), nums2.begin(), nums2.end()); // vector_name.insert(position, iterator1, iterator2)
+        sort(nums1.begin(), nums1.end()); // sorts the merged array
     }
 };
