def02a1f845f09d5a98e246f4230aaa1f1ce4699
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-07-02 20:35:30 +0530

    Time: 115 ms (35.31%), Space: 33.5 MB (11.97%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:  
     int maxDiff(vector<int> v, int length){
+        
             sort(v.begin(),v.end());
+        
             int max_diff=v[0];
+        
             for(int i=1;i<v.size();i++){
                 max_diff=max(max_diff,v[i]-v[i-1]);
             }
 
             max_diff=max(max_diff,length-v.back());
+        
             return max_diff;
 
         }
