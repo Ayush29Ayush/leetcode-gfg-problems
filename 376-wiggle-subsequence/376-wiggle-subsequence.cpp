04a709991f33ac30999715b83e6d3a04363b20e2
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-07-03 09:35:49 +0530

    Time: 2 ms (72.28%), Space: 7.1 MB (43.94%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,24 +1,41 @@
+// class Solution {
+// public:
+//     int wiggleMaxLength(vector<int>& nums) {
+//         if(nums.size()==1)
+//             return 1;
+        
+//         int prev_diff = nums[1] - nums[0];
+        
+//         int counter = (prev_diff != 0) ? 2 : 1;
+        
+//         for(int i=2; i<nums.size(); i++)
+//         {
+//             int curr_diff = nums[i] - nums[i-1];
+            
+//             if((curr_diff > 0 && prev_diff <= 0) || (curr_diff < 0 && prev_diff >= 0))
+//             {
+//                 counter++;
+//                 prev_diff = curr_diff;
+//             }
+//         }
+        
+//         return counter;
+//     }
+// };
+
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        if(nums.size()==1)
-            return 1;
         
-        int prev_diff = nums[1] - nums[0];
+        int n = nums.size();
+        int peak = 1, valley = 1;
         
-        int counter = (prev_diff != 0) ? 2 : 1;
-        
-        for(int i=2; i<nums.size(); i++)
-        {
-            int curr_diff = nums[i] - nums[i-1];
-            
-            if((curr_diff > 0 && prev_diff <= 0) || (curr_diff < 0 && prev_diff >= 0))
-            {
-                counter++;
-                prev_diff = curr_diff;
-            }
+        for(int i=1; i<n; i++){
+            if(nums[i]>nums[i-1])
+                peak=valley + 1;
+            else if(nums[i]<nums[i-1])
+                valley = peak + 1;
         }
-        
-        return counter;
+        return max(peak, valley);
     }
 };
