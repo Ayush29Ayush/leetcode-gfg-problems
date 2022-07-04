4ef40320be458c1c9085373f903158e2e8337c0e
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-07-04 09:42:36 +0530

    Time: 25 ms (80.88%), Space: 17.1 MB (96.69%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/candy/discuss/2234434/C%2B%2B-oror-O(n)-Time-O(1)-Space-oror-Full-Explanation
+
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        int candy = n, i=1;
+        while(i<n){
+            if(ratings[i] == ratings[i-1]){
+                i++;
+                continue;
+            }
+            
+            //For increasing slope
+            int peak = 0;
+            while(ratings[i] > ratings [i-1]){
+                peak++;
+                candy += peak;
+                i++;
+                if(i == n) return candy;
+            }
+            
+            //For decreasing slope
+            int valley = 0;
+            while(i<n && ratings[i] < ratings[i-1]){
+                valley++;
+                candy += valley;
+                i++;
+            }
+            candy -= min(peak, valley); //Keep only the higher peak
+        }
+        return candy;
+    }
+};
