191279ba3e5c53be4ae3464a2350f1b781533937
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-07-14 13:43:28 +0530

    Time: 20 ms (87.84%), Space: 19.6 MB (12.88%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,3 +1,19 @@
+/*Explanation:
+1. Basic idea is that we will sort the array
+2. The element which is present more than size/2 time must appear at the mid of the vector.
+3. Return the middle element.
+*/
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         sort(nums.begin(),nums.end()); // sort the vector
+//         return nums[nums.size()/2];   // middle element will be the most frequent Element
+//     }
+// };
+
+
+// using hashmap -> O(N)
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
