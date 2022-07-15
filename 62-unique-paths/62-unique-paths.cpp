9c3b31c4094b4feab8989d423c400e774cdca722
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-07-15 18:56:34 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (90.25%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,56 @@
+// Recursion se kiya , TLE aa gaya, TC-> exponential
+// class Solution {
+// public:
+//     int countPaths(int i, int j, int m, int n)
+//     {
+//         // Base Case
+//         if(i>=m || j>=n)
+//             return 0;
+//         if(i==(m-1) && j==(n-1))
+//             return 1;
+//         else
+//             return countPaths(i+1, j, m, n) + countPaths(i, j+1, m, n);
+//     }
+//     int uniquePaths(int m, int n) {
+//         int ans = countPaths(0,0,m,n);
+//         return ans;
+//     }
+// };
+
+// DP se kiya, TLE aa gaya, TC -> 
+// class Solution {
+// public:
+//     int countPaths(int i, int j, int m, int n, vector<vector<int>>dp)
+//     {
+//         // Base Case
+//         if(i>=m || j>=n)
+//             return 0;
+//         if(i==(m-1) && j==(n-1))
+//             return 1;
+        
+//         if(dp[i][j]!=-1)
+//             return dp[i][j];
+              
+//         else
+//             return dp[i][j] = countPaths(i+1, j, m, n, dp) + countPaths(i, j+1, m, n, dp);
+//     }
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        
+//         int ans = countPaths(0,0,m,n,dp);
+//         return ans;
+//     }
+// };
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+            int N = n + m - 2;
+            int r = m - 1; 
+            double res = 1;
+            
+            for (int i = 1; i <= r; i++)
+                res = res * (N - r + i) / i;
+            return (int)res;
+    }
+};
