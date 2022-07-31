65040fc21360892e5ca64e1e81f988b113cd538e
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-08-01 00:04:59 +0530

    Time: 31 ms (28.57%), Space: 11.5 MB (29.85%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -8,6 +8,10 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// https://leetcode.com/problems/reverse-nodes-in-k-group/discuss/1914641/C%2B%2B-Easy-Simple-99-faster-recursive-approach-O(1)-space-complexity
+
+// https://youtu.be/fi2vh0nQLi0?t=902
 class Solution
 {
 public:
@@ -17,15 +21,20 @@ public:
             return head;
         if (head == NULL)
             return head;
-        ListNode *current = head, *next = NULL, *prev = NULL, *temp = head;
+        
+        ListNode *current = head, *next = NULL, *prev = NULL; 
+        ListNode *temp = head;
+        
         int size = 0;
         while (temp != NULL)
         {
             size++;
             temp = temp->next;
         }
+        
         if (size < k)
             return head;
+        // Step1 : reverse first k nodes
         int count = 0;
         while (count != k and current != NULL)
         {
@@ -35,10 +44,14 @@ public:
             prev = current;
             current = next;
         }
+        
+        // Step2 : recursion dekhlega aage ka
         if (next != NULL)
         {
             head->next = reverseKGroup(next, k);
         }
+        
+        // Step3 : return head of reversed linked list
         return prev;
     }
 };
