22fc4bb499a882fca74b6c6b1577a6339c619260
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-08-10 16:09:48 +0530

    Time: 12 ms (87.21%), Space: 21.6 MB (88.87%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -9,46 +9,23 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
-//     bool isBST(TreeNode* root, int min, int max)
-//     {
-//         // base case
-//         if(root==NULL)
-//             return true;
+    bool isBST(TreeNode* root, long min, long max)
+    {
+        if(root==NULL) return true;
         
-//         if(root->val >= min && root->val <= max)
-//         {
-//             bool left = isBST(root->left, min, root->val);
-//             bool right = isBST(root->right, root->val, max);
-//             return (left&&right);
-//         }
-//         else 
-//             return false;
-//     }
-//     bool isValidBST(TreeNode* root) {
-//         if(root==NULL) return true;
-//         return isBST(root, LONG_MIN, LONG_MAX);
-//     }
-     bool isBST(TreeNode* root, long min, long max){
-     if(root == NULL || !root){
-         return true;
-     }
-    
-    if( root->val > min && root->val < max){
-        
-        bool left = isBST(root->left, min, root->val);
-        bool right = isBST(root->right, root->val, max);
-        
-        return (left && right);
+        if(root->val > min && root->val < max)
+        {
+            bool left = isBST(root->left, min, root->val);
+            bool right = isBST(root->right, root->val, max);
+            
+            return (left && right);
+        }
+        else
+            return false;
     }
-    else{
-        return false;
+    bool isValidBST(TreeNode* root) {
+        return isBST(root, LONG_MIN, LONG_MAX);
     }
-}    
-
-bool isValidBST(TreeNode* root) {
-    return isBST(root, LONG_MIN, LONG_MAX);
-}
 };
