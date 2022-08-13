ed66eefb4c2386c3b894eb99a3c8ca2f3d72c012
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-08-13 11:11:12 +0530

    Time: 12 ms (10.58%), Space: 15.9 MB (24.81%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,56 +1,29 @@
-// void subset_finder(vector<int>& nums, int index, vector<int> one_subset, vector<vector<int>> ans)
-//     {
-//         // base case
-//         if(index>=nums.size())
-//         {
-//             ans.push_back(one_subset);
-//             return;
-//         }
-//         // take
-//         one_subset.push_back(nums[index]);
-//         subset_finder(nums, index+1, one_subset, ans);
-        
-//         // not take
-//         one_subset.pop_back();
-//         subset_finder(nums, index+1, one_subset, ans);
-//     }
-    
-//     vector<vector<int>> subsets(vector<int>& nums) {
-//         vector<vector<int>> ans;
-//         vector<int> one_subset;
-//         int index = 0;
-//         subset_finder(nums, index, one_subset, ans);
-        
-//         return ans;
-//     }
-
 class Solution {
 private:
-    void solve(vector<int> nums, vector<int> output, int index, vector<vector<int>>& ans) {
-        //base case
-        if(index >= nums.size()) {
-            ans.push_back(output);
-            return ;
+    void subset_finder(vector<int>& nums, int index, vector<int> one_subset, vector<vector<int>> &ans)
+    {
+        // base case
+        if(index>=nums.size())
+        {
+            ans.push_back(one_subset);
+            return;
         }
+        // take
+        one_subset.push_back(nums[index]);
+        subset_finder(nums, index+1, one_subset, ans);
         
-        //exclude
-        solve(nums, output, index+1, ans);
-        
-        //include
-        int element = nums[index];
-        output.push_back(element);
-        solve(nums, output, index+1, ans);
-        
+        // not take
+        one_subset.pop_back();
+        subset_finder(nums, index+1, one_subset, ans);
     }
     
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        
-        vector<vector<int> > ans;
-        vector<int> output;
+        vector<vector<int>> ans;
+        vector<int> one_subset;
         int index = 0;
-        solve(nums, output, index, ans);
-        return ans;
+        subset_finder(nums, index, one_subset, ans);
         
+        return ans;
     }
 };
