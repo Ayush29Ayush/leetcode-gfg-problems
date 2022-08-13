8bfa998c2977e6ed1d0faf43f71ebb3c4e90e4d4
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-08-13 10:42:49 +0530

    Time: 13 ms (7.78%), Space: 16.9 MB (8.55%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,3 +1,29 @@
+// void subset_finder(vector<int>& nums, int index, vector<int> one_subset, vector<vector<int>> ans)
+//     {
+//         // base case
+//         if(index>=nums.size())
+//         {
+//             ans.push_back(one_subset);
+//             return;
+//         }
+//         // take
+//         one_subset.push_back(nums[index]);
+//         subset_finder(nums, index+1, one_subset, ans);
+        
+//         // not take
+//         one_subset.pop_back();
+//         subset_finder(nums, index+1, one_subset, ans);
+//     }
+    
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         vector<int> one_subset;
+//         int index = 0;
+//         subset_finder(nums, index, one_subset, ans);
+        
+//         return ans;
+//     }
+
 class Solution {
 private:
     void solve(vector<int> nums, vector<int> output, int index, vector<vector<int>>& ans) {
@@ -6,19 +32,25 @@ private:
             ans.push_back(output);
             return ;
         }
+        
         //exclude
         solve(nums, output, index+1, ans);
+        
         //include
         int element = nums[index];
         output.push_back(element);
-        solve(nums, output, index+1, ans); 
+        solve(nums, output, index+1, ans);
+        
     }
+    
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
+        
+        vector<vector<int> > ans;
         vector<int> output;
         int index = 0;
         solve(nums, output, index, ans);
-        return ans;   
+        return ans;
+        
     }
 };
