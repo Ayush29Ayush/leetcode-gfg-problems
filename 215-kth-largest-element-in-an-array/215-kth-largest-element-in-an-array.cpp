0bdaebe87b21837777b6a8687d3d041fd5938e02
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-09-09 20:28:07 +0530

    Time: 252 ms (14.89%), Space: 47.3 MB (44.39%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,10 +1,41 @@
 // https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/2180777/C%2B%2B-solution-oror-Simple-oror-Optimized-Solution-oror-Sorting
 
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         sort(nums.begin(),nums.end());
+        
+//         return nums[nums.size()-k];
+//     }
+// };
+
+//! refer -> https://youtu.be/_9F2VgZcvdw?t=1096
+//! Smallest element bola tha toh MAX_HEAP use kiya
+//! If Largest element hota toh MIN_HEAP use karte
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int r = nums.size()-1;
         
-        return nums[nums.size()-k];
+        //! step1
+        for(int i = 0; i < k; i++)
+        {
+            pq.push(nums[i]);
+        }
+        
+        //! step2
+        for(int i = k; i <= r; i++)
+        {
+            if(nums[i] > pq.top())
+            {
+                pq.pop();
+                pq.push(nums[i]);
+            }
+        }
+        
+        //! step3
+        int ans = pq.top();
+        return ans;
     }
 };
