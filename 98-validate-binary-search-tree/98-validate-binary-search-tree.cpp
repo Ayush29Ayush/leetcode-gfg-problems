ff54e82434987356b22bdcb5ddfaf18a5f6ab53f
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-09-10 22:35:41 +0530

    Time: 13 ms (82.86%), Space: 21.8 MB (30.74%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -11,23 +11,54 @@
  */
 
 // refer this if doubts -> https://youtu.be/pDURIj98e0I?t=261
+// class Solution {
+// public:
+//     bool isBST(TreeNode* root, long min, long max)
+//     {
+//         if(root==NULL) return true;
+        
+//         if(root->val > min && root->val < max)
+//         {
+//             bool left = isBST(root->left, min, root->val);
+//             bool right = isBST(root->right, root->val, max);
+            
+//             return (left && right);
+//         }
+//         else
+//             return false;
+//     }
+//     bool isValidBST(TreeNode* root) {
+//         return isBST(root, LONG_MIN, LONG_MAX);
+//     }
+// };
+
+//inorder of bst is always sorted and we can check if it is not sorted then it is not BST.
 class Solution {
 public:
-    bool isBST(TreeNode* root, long min, long max)
-    {
-        if(root==NULL) return true;
-        
-        if(root->val > min && root->val < max)
-        {
-            bool left = isBST(root->left, min, root->val);
-            bool right = isBST(root->right, root->val, max);
-            
-            return (left && right);
+    
+    void inorderTraverse(TreeNode* root,vector<int> &inorder){
+        //base case
+        if(!root){
+            return;
         }
-        else
-            return false;
+        
+        //left call.
+        inorderTraverse(root -> left,inorder);
+        //store while returning from left
+        inorder.push_back(root -> val);
+        //right call
+        inorderTraverse(root -> right,inorder);
     }
+    
     bool isValidBST(TreeNode* root) {
-        return isBST(root, LONG_MIN, LONG_MAX);
+        vector<int> inorder;
+        inorderTraverse(root,inorder);
+        //check if the inorder is sorted.
+        for(int i=0;i<inorder.size()-1;i++){
+            if(inorder[i] >= inorder[i+1]){
+                return false;
+            }
+        }
+        return true;
     }
 };
