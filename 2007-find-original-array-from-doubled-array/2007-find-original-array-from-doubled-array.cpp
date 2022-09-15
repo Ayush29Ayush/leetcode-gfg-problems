0289d15eea44ea37764e2e2d710cd0e9a53bc9e9
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-09-15 10:26:06 +0530

    Time: 1008 ms (14.42%), Space: 144.1 MB (51.60%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,39 +1,34 @@
-// Time Complexity: O(N + KlogK)
-// Space Complexity: O(N)
-// where N is the number of elements in `changed` 
-// and K is the number of unqiue elements in `changed`
 class Solution {
 public:
-    // hashmap approach
     vector<int> findOriginalArray(vector<int>& changed) {
-        // if the length of the input is odd, then return {}
-        // because doubled array must have even length
-        if (changed.size() & 1) return {};
-        // count the frequency of each number
-        unordered_map<int, int> m;
-        for (auto x: changed) m[x]++;
-        vector<int> ans;
-        // sort in ascending order
+        int n = changed.size();
+        // if size is odd means one element more or less so it isn't a doubled array
+        if (n % 2 == 1) return {};
+        
+        //! Step1 : First we take a map, then we sort the given array changed.
+        map<int, int> mp;
         sort(changed.begin(), changed.end());
-        // keep the unique elements only in changed
-        // think of test cases like [0,0,0,0]
-        // alternatively you can handle it like
-        // - check if the frequency of 0 is odd, if so, return {}
-        // - push `0` `m[0] / 2` times to ans
-        changed.erase(unique(changed.begin(), changed.end()), changed.end());
-        // so that we can iterate `changed` from smallest to largest
-        for (auto x : changed) {
-            // if the number of m[x] is greater than than m[x * 2]
-            // then there would be some m[x] left
-            // therefore, return {} here as changed is not a doubled array
-            if (m[x] > m[x * 2]) return {};
-            for (int i = 0; i < m[x]; i++) {
-                // otherwise, we put the element `x` `m[x]` to ans
-                ans.push_back(x);
-                // at the same time we decrease the count of m[x * 2] by 1
-                // we don't need to decrease m[x] by 1 as we won't use it again
-                m[x * 2] -= 1;
-            }
+        
+        vector<int> ans;
+        
+        //! Step2 : Count the freq of each element in changed array and store a key-value pair
+        for (int i = 0; i < n; i++) {
+            mp[changed[i]]++;
+        }
+        
+        //! Step3 : 
+        for (int i = 0; i < n; i++) {
+            
+          // kahi pehle hi iss element ke kaam karke freq 0 naa kardi ho check karo, if true then skip and go to next iteration
+          if (mp[changed[i]] == 0) continue;
+          // check if element ke double ki freq 0 toh nahi ho gayi. Ex-> [1,1,2,3] yahan ek iteration ke baad double ki freq 0 ho jayegi so jab second time 1 pe ayenge toh double ki freq 0 milegi
+          if (mp[changed[i] * 2] == 0) return {};
+            
+          // if upar ke dono cases true nahi hue that means ki doubled array ka format follow ho raha hai 
+          ans.push_back(changed[i]);
+          // element and uske double ki freq minus 1 kardo
+          mp[changed[i]]--;
+          mp[changed[i] * 2]--;
         }
         return ans;
     }
