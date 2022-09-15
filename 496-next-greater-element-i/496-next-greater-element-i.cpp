a9ad89b28254e368be81a6a63138e6420be7d877
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-09-15 12:05:46 +0530

    Time: 86 ms (5.05%), Space: 8.5 MB (79.79%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,46 +1,25 @@
-// class Solution {
-// public:
-//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-//         vector<int> ans(nums1.size());
-        
-//         for(int i = 0; i < nums1.size(); i++){
-//             for(int j = 0; j < nums2.size(); j++){
-//                 if(nums1[i]==nums2[j])
-//                     {
-//                         for(int k = j; k<nums2.size(); k++)
-//                         {
-//                             if(nums2[k] > nums2[j])
-//                             {
-//                                 ans.push_back(nums2[k]);
-//                                 break;
-//                             }
-//                         }
-//                         ans.push_back(-1);
-//                         break;
-//                     }
-//                 }
-//             }
-//         return ans;
-//     }
-// };
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         vector<int> ans;
-        for(int i=0; i<nums1.size(); i++){
-            for(int j=0; j<nums2.size(); j++){
-                if(nums1[i] == nums2[j]){
-                    int m= -1;
-                    for(int k=j; k<nums2.size(); k++){
-                        if(nums2[k]>nums2[j]){
-                            m=nums2[k];
-                            break;
-                        }  
+        
+        for(int i = 0; i < nums1.size(); i++){
+            for(int j = 0; j < nums2.size(); j++){
+                if(nums1[i]==nums2[j])
+                    {
+                    int next = -1;
+                        for(int k = j; k<nums2.size(); k++)
+                        {
+                            if(nums2[k] > nums2[j])
+                            {
+                                next = nums2[k];
+                                break;
+                            }
+                        }
+                        ans.push_back(next);
                     }
-                    ans.push_back(m);
                 }
             }
-        }
         return ans;
     }
 };
