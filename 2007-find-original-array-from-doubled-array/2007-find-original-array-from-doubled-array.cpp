23324d5591c26ec6a35831d89f9f991c79d0475a
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-09-15 10:30:09 +0530

    Time: 949 ms (17.83%), Space: 144.1 MB (51.60%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -24,6 +24,8 @@ public:
           // kahi pehle hi iss element ke kaam karke freq 0 naa kardi ho check karo, if true then skip and go to next iteration
           if (mp[changed[i]] == 0) continue;
           // check if element ke double ki freq 0 toh nahi ho gayi. Ex-> [1,1,2,3] yahan ek iteration ke baad double ki freq 0 ho jayegi so jab second time 1 pe ayenge toh double ki freq 0 milegi
+            
+          // since changed is sorted, the only pair we can make for changed[i] is changed[i] * 2. Therefore, if the count of the target pair is 0, we couldn't make the pair, then we return {}.
           if (mp[changed[i] * 2] == 0) return {};
             
           // if upar ke dono cases true nahi hue that means ki doubled array ka format follow ho raha hai 
