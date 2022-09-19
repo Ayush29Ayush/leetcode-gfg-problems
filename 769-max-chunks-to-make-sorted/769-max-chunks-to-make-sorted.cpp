78cf740e518ac571c50a13495164bc9c3917b0bd
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-09-19 15:40:34 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (28.75%) - LeetHub

---
+++ 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.cpp
@@ -0,0 +1,20 @@
+//! we will use chaining technique -> will see till where the impact is possible
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int ChunkCount = 0;
+        int MaxElementImpact = 0;
+        int n = arr.size();
+        
+        //! Step1 : Traverse the array and take max of element and impact_element 
+        for(int i=0; i<n; i++)
+        {
+            MaxElementImpact = max(MaxElementImpact, arr[i]);
+            //! Step2 : When max_impact_element equals index means impact by these elements is till this place only, going furthur will move to the next partition. So do chuck++
+            if(MaxElementImpact == i)
+                ChunkCount++;
+        }
+        
+        return ChunkCount;
+    }
+};
