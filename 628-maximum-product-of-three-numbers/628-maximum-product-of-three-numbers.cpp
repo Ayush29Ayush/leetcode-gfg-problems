2276b698a093dce77dd5c355d9beb4af6e66b9c1
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-09-20 11:56:54 +0530

    Time: 106 ms (11.78%), Space: 27.7 MB (36.10%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
+        //sort to get ascending order so that largest numbers are at the end and smallest or negatives will be at start
+        //Ex - {2,-3,3,-1,4} => {-3,-1,2,3,4}
         sort(nums.begin(), nums.end());
         int n = nums.size();
+        
+        // combination1 will be product of largest 3 numbers
         int combination1 = nums[n-1]*nums[n-2]*nums[n-3];
+        // combination2 => lets take this test case as demo
+        // prod of 2 negatives will make it positive and then multiplying that with the largest element can give the                largest product possible
+        // Ex -> {-10,-9,1,2,3}
         int combination2 = nums[0]*nums[1]*nums[n-1];
         
-                
+        // now check which combination is maximum
         return max(combination1,combination2);
     }
 };
