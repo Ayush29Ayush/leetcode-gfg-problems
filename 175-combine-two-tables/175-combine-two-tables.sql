55d97f7b14273060756778808e27632673200769
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-10-01 20:48:26 +0530

    Time: 452 ms (59.29%), Space: 0B (100.00%) - LeetHub

--- 175-combine-two-tables/175-combine-two-tables.sql
+++ 175-combine-two-tables/175-combine-two-tables.sql
@@ -1,3 +1,8 @@
 # Write your MySQL query statement below
+-- left join kiya because left side ka sab chahiye but right side ka agar nahi mile toh null kar dena
+# SELECT firstname, lastname, city, state
+# FROM person LEFT JOIN Address ON Person.personId = Address.personId
+
 SELECT firstname, lastname, city, state
-FROM person LEFT JOIN Address ON Person.personId = Address.personId
+FROM person p LEFT JOIN Address a
+ON p.personId = a.personId
