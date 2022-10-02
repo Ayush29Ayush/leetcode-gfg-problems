e85abd5a71acd337e1ca9fcecaa3720354455f69
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-10-02 19:32:26 +0530

    Time: 312 ms (22.03%), Space: 111.7 MB (46.76%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,34 +1,34 @@
-// https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/discuss/1865373/C%2B%2B-oror-2-Approaches-oror-Explained-With-Algorithm-oror-Easy-and-Simple
-
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        int n = tops.size();
+        int size = tops.size();
+        vector<int> freq1(7,0); // freq1 for tops
+        vector<int> freq2(7,0); // freq2 for bottoms
+        vector<int> freq3(7,0); // freq3 for equal elements in tops and bottom
         
-        // faceA for counting the occurence in tops
-		// faceB for counting occurence of numbers in bottoms
-        // same for counting when both tops and bottoms have same occurences
-        vector<int> faceA(7), faceB(7), same(7);
-        
-        for(int i = 0; i < n; ++i)
+        // teeno freq vectors mein individual elements ki freq store kar diya
+        for(int i=0; i<size; i++)
         {
-            ++faceA[tops[i]];
-            ++faceB[bottoms[i]];
+            freq1[tops[i]]++;
+            freq2[bottoms[i]]++;
             
-            if(tops[i] == bottoms[i])
-                ++same[tops[i]];
+            if(tops[i]==bottoms[i])
+                freq3[tops[i]]++;
         }
         
-        int minRotation = INT_MAX;
+        int ans = INT_MAX;
         
-		// all possibilities from 1 to 6
-        for(int i = 1; i<=6; ++i)
+        for(int i = 1; i<=6; i++)
         {
-            if(faceA[i] + faceB[i] - same[i] == n)
-                minRotation = min(minRotation , min(faceA[i],faceB[i]) - same[i]);
+            if(freq1[i] + freq2[i] - freq3[i] == size)
+            {
+                int minimumswap = min(freq1[i],freq2[i]) - freq3[i];
+                ans = min(ans,minimumswap);
+                return ans;
+            }
         }
         
-        return minRotation == INT_MAX ? -1 : minRotation;
+        return -1;
         
     }
 };
