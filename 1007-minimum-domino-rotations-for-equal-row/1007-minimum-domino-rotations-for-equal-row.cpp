90d7b792ef1ce87d422e925b35036a089c4db53d
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-10-02 20:35:04 +0530

    Time: 145 ms (94.37%), Space: 111.6 MB (85.68%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,3 +1,13 @@
+//! DRY RUN
+// tops = [2,1,2,4,2,2], bottoms = [5,2,6,2,3,2]
+// So, freq1 = [0,1,4,0,1,0,0]
+//     freq2 = [0,0,3,1,0,1,1]
+//     freq3 = [0,0,1,0,0,0,0]
+
+// we observe that 2 is having freq1[i] + freq2[i] - freq3[i] == size
+// jis side ki freq kam hai usko swap karana hai so here in freq2 ka 3 is less than freq1 ka 4
+// and subtract the common wala freq
+// and then take the min
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
@@ -15,8 +25,6 @@ public:
             if(tops[i]==bottoms[i])
                 freq3[tops[i]]++;
         }
-        
-        
         
         // all possibilities from 1 to 6
         for(int i = 1; i<=6; i++)
