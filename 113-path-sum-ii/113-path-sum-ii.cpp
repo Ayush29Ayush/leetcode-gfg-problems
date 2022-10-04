5c64bff662b8e8cd67fce26c65f61f1d707da8c2
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-10-04 12:27:37 +0530

    Time: 35 ms (20.52%), Space: 38.6 MB (19.01%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -13,14 +13,20 @@ class Solution {
 public:
      void solve(TreeNode *root,int targetSum,vector<int> ds,vector<vector<int>> &ans)
     {
+        // base case
         if(root==NULL) return;
+        // if not base case then add to ds and targetSum = difference to target and root value
         ds.push_back(root->val);
         targetSum=targetSum-root->val;
+        
+        // when targetSum is finally 0 means we have got our reqd elements in an 1D vec, so push them to 2D vec 
         if(targetSum==0 && root->left==NULL && root->right==NULL)
         {
             ans.push_back(ds);
             return;
         }
+        
+        // if non of the above condition is satisfied then again call for left and right 
         solve(root->left,targetSum,ds,ans);
         solve(root->right,targetSum,ds,ans);
     }
