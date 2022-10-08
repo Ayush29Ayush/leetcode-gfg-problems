755ec34ffa9ca4380a9122cf5b3479c0bb2afa52
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-10-08 15:32:22 +0530

    Time: 356 ms (73.23%), Space: 16.4 MB (48.38%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -1,37 +1,48 @@
+//Ek element ko fix karo and aage ke do elements ko dhundho which will give sum closest to target
+
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        int diff=INT_MAX;
-        int ans=0;
+        int n = nums.size();
+        int min_diff = INT_MAX;
+        int ans = 0;
         
-        //optimized approach ,check if there are 3 
-        for(int i=0;i<n-2;i++){
-            // if(i>0 && nums[i]==nums[i-1])
-            //     continue;
+        sort(nums.begin(), nums.end());
+        
+        for(int i = 0; i<n; i++)
+        {
+            // fixing the first element 
+            int first = nums[i];
             
-            int start=i+1;
-            int end=n-1;
-            while(start<end){
-                int sum=0;
-                sum=nums[i]+nums[start]+nums[end];
-                if(sum>target){
-                    end--;
-                }
-                else if(sum<target){
-                    start++;
-                }
-                else{
+            // now apply two pointer approach
+            int start = i+1;
+            int end = n-1;
+            
+            while(start<end)
+            {
+                int sum = 0;
+                sum = first + nums[start] + nums[end];
+                
+                if(sum == target) // that means dono ko subtract karke 0 aa gaya, isse closest aur kuch nahi ho sakta
+                {
                     return sum;
                 }
                 
-                if(abs(sum-target)<diff){
-                    ans=sum;
-                    diff=abs(sum-target);
-                }
+                // yahan pointer ko aage piche karo
+                if(sum > target)
+                    end--;
+                else if(first + nums[start] + nums[end] < target)
+                    start++;
+                
+                // yahan agar koi diff mila jo min_diff se chota ahi toh usse update kardo
+                if(abs(sum-target) < min_diff)
+                {
+                    min_diff  = abs(sum-target);
+                    ans = sum;
+                }    
             }
         }
+        
         return ans;
     }
 };
