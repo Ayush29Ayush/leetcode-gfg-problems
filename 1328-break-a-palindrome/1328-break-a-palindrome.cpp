0e959e9dbd1d2f087359c75f71beec1f30ea4299
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-10-10 13:01:22 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (5.00%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,7 +1,17 @@
+// why i==n/2 is used ???
 // when i==n/2 then
 // i is at mid and the length is odd
 // "aabaa" if you character 'b' to some other character then it is still Palindrome, no need to check on that index that's why I put a continue statement.
 // "abba" but in this, you change the character to mid-index then it is not palindrome
+
+// why s[n-1]='b'; is used ???
+// because 'a' is the smallest character. if we get a character other than the 'a' then we replace it with 'a'
+// and this is for corner case s[n-1]='b'
+// if your string contains character only 'a' then we have only a way to replace the last character with 'b' to remove its palindrome property
+// eg:
+// intput string : "aaaaaaaaaa"
+// to remove its palindrome property we need to replace a character but after replacing the character string must be lexicographically smaller that's why s[n-1]='b'
+// output :"aaaaaaaaab"
 
 class Solution {
 public:
