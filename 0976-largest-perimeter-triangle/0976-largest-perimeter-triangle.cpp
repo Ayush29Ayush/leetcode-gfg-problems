5548ab4bdc1460f0407613b1f8dd7d9db31e209c
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-10-12 15:23:27 +0530

    Time: 43 ms (87.82%), Space: 21.9 MB (71.51%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -1,3 +1,10 @@
+// Sort the array
+// Apply the geometry principe of triangle "The sum of the length of the two sides of a triangle is greater than the length of the third side" A<B+C
+// Do the reverse iteration.
+// Apply these logic nums[i] + nums[i+1]>nums[i+2]
+// nums[i] + nums[i+1]>nums[i+2] if it is true then return nums[i] + nums[i+1] + nums[i+2]
+// else return 0
+
 class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
