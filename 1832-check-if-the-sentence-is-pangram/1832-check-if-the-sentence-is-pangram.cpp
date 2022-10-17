5b302f4c8f41416831f32133342c63601966d8c4
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-10-17 10:42:56 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (82.91%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,16 +1,46 @@
+// //! Using sets
+// class Solution {
+// public:
+//     bool checkIfPangram(string sentence) {
+//         unordered_set<char> uset;
+        
+//         for(int i=0; i<sentence.size(); i++)
+//         {
+//             uset.insert(sentence[i]);
+//         }
+        
+//         if(uset.size()==26)
+//             return true;
+//         else
+//             return false;
+//     }
+// };
+
+//! Using map
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        unordered_set<char> uset;
+        // unordered_map<char,int> mp;
+        vector<int> freq(26,0);
         
-        for(int i=0; i<sentence.size(); i++)
+        for(int i = 0; i<sentence.size(); i++)
         {
-            uset.insert(sentence[i]);
+            // mp[sentence[i]]++;
+            freq[sentence[i]-'a']++;
         }
         
-        if(uset.size()==26)
-            return true;
-        else
-            return false;
+        // for(auto i : mp)
+        // {
+        //     if(i.second==0)
+        //         return false;
+        // }
+        
+        for(int i = 0; i<freq.size(); i++)
+        {
+            if(freq[i]==0)
+                return false;
+        }
+        
+        return true;
     }
 };
