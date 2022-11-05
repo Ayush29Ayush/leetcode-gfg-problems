8af75738991d0c3a07a6616f5d1c199a74f8dc3a
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-11-05 22:25:33 +0530

    Time: 13 ms (51.62%), Space: 9.1 MB (10.19%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,26 @@
+//! STRIVER memoization code
+class Solution {
+public:
+    int fmemo(int i, int j, vector<vector<int>>& triangle, int n, vector<vector<int>>& dp)
+    {
+        //base case
+        if(i==n-1)
+            return triangle[n-1][j];
+
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+
+        int down = triangle[i][j] + fmemo(i+1, j, triangle, n, dp);
+        int diagonal = triangle[i][j] + fmemo(i+1, j+1, triangle, n, dp);
+
+        return dp[i][j] = min(down, diagonal);
+    }
+    
+    int minimumTotal(vector<vector<int>>& triangle) 
+    {
+        int n = triangle.size();
+        vector<vector<int>> dp(n, vector<int> (n, -1));
+        return fmemo(0,0,triangle,n,dp);
+    }
+};
+
