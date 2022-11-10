1d05c3e87ad1ebc15a7b00a7cbc7291aba08420a
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-11-10 11:30:11 +0530

    Time: 8 ms (99.68%), Space: 10.9 MB (66.38%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,18 +1,18 @@
-class Solution
-{
+// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/discuss/2798136/LeetCode-The-Hard-Way-Explained-Line-By-Line-(C%2B%2B-Python-Java-Go-Kotlin-Rust)
+class Solution {
 public:
-    string removeDuplicates(string s)
-    {
-        for (int i = 0; i < s.length(); i++)
-        {
-            if (s[i] == s[i + 1])
-            {
-                s.erase(i, 2);
-                i = i - 2;
-                if (i <= 0)
-                    i = -1;
-            }
+    string removeDuplicates(string s) {
+        // use string here instead of Stack<char>
+        // so that we don't need to build the final string again
+        string ans;
+        for (auto c : s) {
+            // if the current character is same as the last one in `ans`
+            // then we cannot push it to `ans`
+            // we remove the one in `ans`
+            if (ans.size() && ans.back() == c) ans.pop_back();
+            // otherwise, add the current character to `ans`
+            else ans.push_back(c);
         }
-        return s;
+        return ans;
     }
 };
