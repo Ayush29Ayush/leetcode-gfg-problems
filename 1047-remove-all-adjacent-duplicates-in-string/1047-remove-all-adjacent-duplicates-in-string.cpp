b2635ff3aee5d9c1cf1e473c21a238af1de62817
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-11-11 12:02:44 +0530

    Time: 81 ms (25.92%), Space: 11.6 MB (24.57%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,18 +1,47 @@
 // https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/discuss/2798136/LeetCode-The-Hard-Way-Explained-Line-By-Line-(C%2B%2B-Python-Java-Go-Kotlin-Rust)
+// class Solution {
+// public:
+//     string removeDuplicates(string s) {
+//         // use string here instead of Stack<char>
+//         // so that we don't need to build the final string again
+//         string ans;
+//         for (auto c : s) {
+//             // if the current character is same as the last one in `ans`
+//             // then we cannot push it to `ans`
+//             // we remove the one in `ans`
+//             if (ans.size() && ans.back() == c) ans.pop_back();
+//             // otherwise, add the current character to `ans`
+//             else ans.push_back(c);
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     string removeDuplicates(string s) {
-        // use string here instead of Stack<char>
-        // so that we don't need to build the final string again
-        string ans;
-        for (auto c : s) {
-            // if the current character is same as the last one in `ans`
-            // then we cannot push it to `ans`
-            // we remove the one in `ans`
-            if (ans.size() && ans.back() == c) ans.pop_back();
-            // otherwise, add the current character to `ans`
-            else ans.push_back(c);
+        stack<char> st;
+        
+        for(auto element : s)
+        {
+            if(!st.empty() && st.top()==element)
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(element);
+            }
         }
+        
+        string ans;
+        while(!st.empty())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
