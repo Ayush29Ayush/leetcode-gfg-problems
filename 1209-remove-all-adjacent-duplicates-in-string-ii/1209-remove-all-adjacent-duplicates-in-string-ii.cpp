a58b52b3c29a170ebc88a1f0daf46caf80a9d90a
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-11-11 12:07:55 +0530

    Time: 10 ms (99.54%), Space: 9.4 MB (90.84%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/discuss/1161129/Multiple-Approach-Explained-w-Easy-Commented-Solutions-or-Beats-100
+
+class Solution {
+public:
+     string removeDuplicates(string& s, int k) {
+        vector<pair<char, short> > stk {{'#', 0}}; // vector required to rebuild ans at end - will function same as stack elsewhere
+        for(const auto c : s){
+            if(stk.back().first == c) ++stk.back().second;
+            else stk.push_back({c, 1});
+            // if consecutive count equals k, then delete from stack, so we won't include it while rebuilding string at end
+            if(stk.back().second == k) stk.pop_back();
+        }
+        s.clear();
+        for(const auto& [character, count] : stk) s.append(count, character);
+        return s;
+    }
+};
