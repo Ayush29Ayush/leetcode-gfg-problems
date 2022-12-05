78791ff3ad2a028c6c373836f56407b75cf83b83
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-12-05 22:55:06 +0530

    Time: 80 ms (12.30%), Space: 13.2 MB (12.31%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -35,8 +35,26 @@ class Solution {
             int longestCommonSubsequence(string text1, string text2) {
                 int n = text1.size();
                 int m = text2.size();
-                vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
-                pre(n,m,text1,text2,dp);
+                vector<vector<int>> dp(n,vector<int> (m,-1));
+                pre(n-1,m-1,text1,text2,dp);
                 return dp[n-1][m-1];
             }
         };
+
+//! MEMOIZATION with RIGHT SHIFT striver
+// class Solution {
+//         public:
+//             int pre(int n,int m,string &s1,string &s2,vector<vector<int>> &dp){
+//                 if(n == 0 or m == 0) return 0;
+//                 if(dp[n][m]!=-1) return dp[n][m];
+//                 if(s1[n-1] == s2[m-1]) return dp[n][m] = pre(n-1,m-1,s1,s2,dp) + 1;
+//                 else return dp[n][m] = max(pre(n-1,m,s1,s2,dp),pre(n,m-1,s1,s2,dp));
+//             }
+//             int longestCommonSubsequence(string text1, string text2) {
+//                 int n = text1.size();
+//                 int m = text2.size();
+//                 vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
+//                 cout<<pre(n,m,text1,text2,dp)<<endl;
+//                 return dp[n][m];
+//             }
+//         };
