db61cbb7a30ad010ef082d427ca8434adfd0c1e8
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-12-06 08:26:22 +0530

    Time: 4 ms (72.79%), Space: 7.6 MB (67.12%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    //! Solution 1 : Since Alice is starting first, from wherever she starts the winner will always be Alice since her sum will be greater than Bob
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
