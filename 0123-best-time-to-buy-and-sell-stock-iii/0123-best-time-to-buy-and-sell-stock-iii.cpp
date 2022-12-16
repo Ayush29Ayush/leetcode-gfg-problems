6a2802f49ab5d96b215abcae06edd715b24e6a23
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-12-16 19:50:08 +0530

    Time: 784 ms (39.54%), Space: 207.1 MB (34.29%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -28,9 +28,36 @@ public:
     
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>> (2,vector<int>(3,-1)));
+        // vector<vector<vector<int>>> dp(n,vector<vector<int>> (2,vector<int>(3,-1)));
         
-        int ans = getAns(prices,n,0,1,2,dp);
-        return ans;
+        //! TABULATION
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>> (2,vector<int>(3,0)));
+        
+        // int ans = getAns(prices,n,0,1,2,dp);
+        // return ans;
+        
+        // base case - 1 (cap==0 return 0)
+        // for(int ind = 0; ind<n-1; ind++)
+        //     for(int buy = 0; buy<=1; buy++)
+        //         dp[ind][buy][0] = 0;
+        // base case - 2 (ind==n return 0)
+        // for(int buy = 0; buy<=1; buy++)
+        //     for(int cap = 0; cap<=2; cap++)
+        //         dp[n][buy][cap] = 0;
+        
+        for(int ind=n-1; ind>=0; ind--)
+        {
+            for(int buy=0; buy<=1; buy++)
+            {
+                for(int cap=1; cap<=2; cap++) // for(int cap=0; cap<=2; cap++) since cap=0 mein 0 value hai acc to base case
+                {
+                    if(buy==1)
+                        dp[ind][buy][cap] = max(0 + dp[ind+1][1][cap], -prices[ind] + dp[ind+1][0][cap]);
+                    if(buy==0)
+                        dp[ind][buy][cap] = max(0 + dp[ind+1][0][cap], +prices[ind] + dp[ind+1][1][cap-1]);
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
