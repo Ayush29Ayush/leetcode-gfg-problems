97e5bea5eac50654ef2c9a64cf3bdb249861effd
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2022-12-28 12:45:14 +0530

    Time: 759 ms (66.55%), Space: 105.1 MB (28.79%) - LeetHub

---
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/remove-stones-to-minimize-the-total/discuss/2961092/C%2B%2B-or-Heap-(Priority-Queue)-or-Intuitive-Solution
+class Solution 
+{
+public:
+    int minStoneSum(vector<int>& piles, int k) 
+    {
+        //use max-heap priority queue to get maximum values from piles
+        priority_queue<int> pq; 
+        for(auto &it:piles) pq.push(it); //put all elements in the priority queue
+
+        int n;
+        while(k-- && !pq.empty()) //apply operation k times
+        {
+            n = pq.top(); pq.pop(); //take max element
+            n = n-n/2; //remove stones
+            pq.push(n); //put remaining stones
+        }
+
+        int sum=0;
+        while(!pq.empty()) //count total number of stones
+        {
+            sum += pq.top(); pq.pop();
+        }
+        
+        return sum;
+    }
+};
