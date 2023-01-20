65ab30b88f34a34711be83eb8db9b18f3236021e
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2023-01-20 16:07:31 +0530

    Time: 1019 ms (26.99%), Space: 117.2 MB (9.51%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-         priority_queue<pair<double, pair<int, int>>> pq;
+         priority_queue<pair<double, pair<int, int>>> pq; // MAX_HEAP
          int n = arr.size();
          for(int i = 0 ; i < n - 1; i++){
             for(int j = i + 1 ; j < n; j++){
