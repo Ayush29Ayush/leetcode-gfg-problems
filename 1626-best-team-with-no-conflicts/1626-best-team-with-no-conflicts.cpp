a724d8aff99bc9676eb0a47b4489de1943c6aa96
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2023-01-31 11:01:46 +0530

    Time: 152 ms (88.64%), Space: 19.4 MB (79.87%) - LeetHub

---
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/best-team-with-no-conflicts/discuss/3120787/Day-31-oror-C%2B%2B-oror-DP-Easiest-Beginner-Friendly-Sol-oror-O(n2)-time-and-O(n)-space
+
+// Use DP -> LIS
+class Solution {
+public:
+    int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+        int n = scores.size();
+        int dp[n], ans = 0;
+        vector<pair<int, int>> players;
+        for(int i = 0; i < n; i++) 
+            players.push_back({ages[i], scores[i]});
+        sort(players.begin(), players.end());
+        for(int i = 0; i < n; i++) {
+            dp[i] = players[i].second;
+            for(int j = 0; j < i; j++) {
+                if(players[j].second <= players[i].second)
+                    dp[i] = max(dp[i], dp[j] + players[i].second);
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
