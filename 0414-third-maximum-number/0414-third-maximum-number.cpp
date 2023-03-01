98c160ef045e620705f60f7c55b683141bd3316b
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2023-03-01 17:55:02 +0530

    Time: 3 ms (96.25%), Space: 9.2 MB (69.69%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,3 +1,6 @@
+// Approach1: Use set and store all unique elements. Then vector<int> v(s.begin(), s.end()) se set to vector karke sort karo vector ko and return n-3 if size>=3 else n-1 return karna
+
+// Approach2: Use sort function and count to check 3rd largest
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
