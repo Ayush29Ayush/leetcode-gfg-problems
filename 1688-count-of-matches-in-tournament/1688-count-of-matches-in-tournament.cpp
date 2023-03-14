ba91f7943c39c0244b41e77524d4f61b61807226
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2023-03-14 19:28:14 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (8.56%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        return n-1;
+    }
+};
+
+// class Solution {
+// public:
+//     int numberOfMatches(int n) {
+//       return n == 1 ? 0 : n / 2 + numberOfMatches(n / 2 + (n%2));
+//     }
+// };
