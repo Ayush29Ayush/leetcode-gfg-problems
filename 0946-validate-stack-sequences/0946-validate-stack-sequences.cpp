937ee77fcb9a9ade87fd65b98cef21896fdad2cd
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2023-04-13 14:52:40 +0530

    Time: 7 ms (85.57%), Space: 15.3 MB (86.87%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -1,22 +1,36 @@
-// https://leetcode.com/problems/validate-stack-sequences/discuss/1853109/C%2B%2B-oror-2-Approaches-oror-Explained-With-Algorithm-oror-Easy-and-Simple
+/*
+    YOUTUBE VIDEO for reference : https://www.youtube.com/watch?v=HEh7EtNSXtg
+    Company Tags                : AMAZON
+    Leetcode Link               : https://leetcode.com/problems/validate-stack-sequences/
+*/
+
+
+//1. Each distinct value
+//2. Initially stack is empty toh 1st step will be to push
+//3. popped is permutation of pushed
+//4. length of pushed and popped are same
 
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        stack<int> s ;   // an empty stack
-        int j = 0;
-        for(int i= 0; i<pushed.size(); ++i)
+        // initialize a empty stack
+        stack<int> st;
+        int n = pushed.size();
+        int i=0, j=0;
+        
+        while(i<n && j<n)
         {
-            s.push(pushed[i]);
+            st.push(pushed[i]);
             
-			// check if pushed values is next to pe popped out.
-            while(!s.empty() && s.top() == popped[j])
+            // check if pushed values is next to pe popped out.
+            while(!st.empty() && st.top()==popped[j] && j<n)
             {
-                s.pop();
-                ++j;
+                st.pop();
+                j++;
             }
+            i++;
         }
-		// if stack is empty means sequence is correct.
-        return s.empty();
+        // if stack is empty means sequence is correct.
+        return (st.empty());
     }
 };
