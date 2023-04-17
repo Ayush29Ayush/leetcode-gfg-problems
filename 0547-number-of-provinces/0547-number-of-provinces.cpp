8d50ba0f6b9167166fd0637f86ccba7d2628afb6
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2023-04-17 14:55:52 +0530

    Time: 22 ms (83.35%), Space: 15.4 MB (10.23%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,3 +1,4 @@
+//! Approach1 => Khudka Adjecency list banao and DFS se solve kardo
 class Solution {
 public:
     void DFS(unordered_map<int,vector<int>> &adj, int u, vector<bool> &visited)
@@ -49,3 +50,37 @@ public:
         return count;
     }
 };
+
+//Approach-2 : [ Using DFS - Using given input graph]
+// class Solution {
+// public:
+//     int n;
+//     void dfs(vector<vector<int>> &adj, int u, vector<bool>& visited) {
+//         visited[u] = true;
+        
+//         //Visit neighbours
+//         for(int v = 0; v < n; v++) {
+//             if(adj[u][v] == 1 && !visited[v]) {
+//                 dfs(adj, v, visited);
+//             }
+//         }
+//     }
+    
+    
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         n = isConnected.size();
+        
+//         vector<bool> visited(n, false);
+//         int count = 0;
+        
+//         for(int i = 0; i<n; i++) {
+//             if(!visited[i]) {
+//                 count++;
+//                 dfs(isConnected, i, visited);
+//             }
+//         }
+        
+//         return count;
+        
+//     }
+// };
