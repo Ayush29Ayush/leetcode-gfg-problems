d25e8270308e44b39359eb254ff0d2881ef27a52
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2023-04-17 15:02:18 +0530

    Time: 13 ms (32.44%), Space: 8 MB (84.31%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int srcx = 0 ; 
+        int srcy = 0 ; 
+
+        for (int i = 0 ; i < moves.length() ; i++)
+        {
+            if (moves[i] == 'R')
+            {
+                srcx++ ;
+            }
+            else if (moves[i] == 'L')
+            {
+                srcx-- ;
+            }
+            else if (moves[i] == 'U')
+            {
+                srcy++;
+            }
+            else
+            {
+                srcy--; 
+            }
+        }
+        if (srcx == 0 && srcy == 0)
+        {
+            return true ; 
+        }
+        return false ; 
+    }
+};
