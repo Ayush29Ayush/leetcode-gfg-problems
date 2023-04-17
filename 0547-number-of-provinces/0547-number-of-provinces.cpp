46a0e53febad656af2d92f7cbfbe6c6c76a4d018
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2023-04-17 14:30:52 +0530

    Time: 26 ms (58.07%), Space: 15.6 MB (8.78%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,34 +1,51 @@
 class Solution {
-private:
-    void dsf(int node , vector<int> adjLs[] , vector<int> &vis){
-        vis[node] = 1;
-        for(auto it : adjLs[node]){
-            if(!vis[it]){
-                dsf(it , adjLs , vis);
+public:
+    void DFS(unordered_map<int,vector<int>> &adj, int u, vector<bool> &visited)
+    {
+        // visited mark karo u ko
+        visited[u] = true;
+        
+        // Ab DFS lagao u ke adj list pe
+        for(auto v : adj[u])
+        {
+            if(!visited[v])
+            {
+                DFS(adj,v,visited);
             }
         }
     }
-public:
+    
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<int> adjLs[n];
-        vector<int> vis(n,0);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i!=j && isConnected[i][j]==1) {
-                    adjLs[i].push_back(j);
-                    adjLs[j].push_back(i);
+        
+        vector<bool> visited(n,false);
+        
+        // 1. Make graph by making adjecency list. i means u and j means v
+        unordered_map<int,vector<int>> adj;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(isConnected[i][j]==1) // condition given in ques
+                {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
         
         int count = 0;
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
+        // 2. Ab bass DFS call lagado on adj list
+        // 1 <= n <= 200 given in ques toh for(int u=1; u<=n; u++) bhi kar sakte they
+        for(int u=0; u<n; u++)
+        {
+            if(!visited[u])
+            {
+                DFS(adj,u,visited);
                 count++;
-                dsf(i ,adjLs , vis);
             }
         }
+        
         return count;
     }
 };
