0b592ac9a82bfe2ab53bf2cdde0da26a04cd6a5c
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2023-04-25 00:09:22 +0530

    Time: 1195 ms (18.56%), Space: 0B (100.00%) - LeetHub

---
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+
+# Here we will use new window function called LEAD() which gives us the consecutive X units forward's ans.
+# humme voh number chahiye jiska present bhi N ho, next wala bhi N ho and next to next wala bhi N ho. 3 consecutive number same ho toh return karna hai\
+
+# for reference => https://www.youtube.com/watch?v=iNaLjT9ybYI
+
+
+# 2. WITH newtable AS
+# 1. (SELECT *, LEAD(num,1) OVER() AS next1, LEAD(num,2) OVER() AS next2next FROM Logs)
+
+WITH newtable AS
+(SELECT *, LEAD(num,1) OVER() AS next1, LEAD(num,2) OVER() AS next2next FROM Logs)
+
+# 3. neeche ka pura
+SELECT DISTINCT num AS ConsecutiveNums
+FROM newtable
+WHERE num = next1 
+AND num = next2next
+
