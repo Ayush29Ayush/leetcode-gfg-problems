0c5f7026e7e9d3abfc343634936fe89b59dc57bd
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2023-04-25 21:29:37 +0530

    Time: 7 ms (22.57%), Space: 8.5 MB (11.79%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,4 +1,5 @@
 // refer this if needed for DRY RUN => https://www.youtube.com/watch?v=qOhKCAm9-sw&t=242s
+// 1st iteration mein smallest subract, 2nd iteration mein 2nd smallest subtract, Nth iteration mein Nth smallest subtract so size() ke barabar ans ayega
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
