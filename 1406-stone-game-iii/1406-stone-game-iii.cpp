20624195bc0182b8d3401e021879feaa99b09c3f
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2023-05-28 00:03:30 +0530

    Time: 312 ms (28.67%), Space: 137.6 MB (43.60%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -0,0 +1,44 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=KI8suf35r38
+    Company Tags                : GOOGLE, META
+    Leetcode Link               : https://leetcode.com/problems/stone-game-iii/
+*/
+
+//Approach-1 (Using simple resurion + memoization)
+class Solution {
+public:
+    int n;
+    vector<int> t;
+    
+    int solve(vector<int>& stoneValue, int i) {
+        if(i == n)
+            return 0;
+        
+        if(t[i] != -1)
+            return t[i];
+        
+        t[i] = stoneValue[i] - solve(stoneValue, i+1);
+        
+        if(i+1 < n)
+            t[i] = max(t[i], stoneValue[i] + stoneValue[i+1] - solve(stoneValue, i+2));
+        
+        if(i+2 < n)
+            t[i] = max(t[i], stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - solve(stoneValue, i+3));
+        
+        return t[i];
+    }
+    
+    string stoneGameIII(vector<int>& stoneValue) {
+        n = stoneValue.size();
+        t.resize(n+1, -1);
+        
+        int diff =  solve(stoneValue, 0);
+        
+        if(diff < 0)
+            return "Bob";
+        else if(diff > 0)
+            return "Alice";
+        
+        return "Tie";
+    }
+};
