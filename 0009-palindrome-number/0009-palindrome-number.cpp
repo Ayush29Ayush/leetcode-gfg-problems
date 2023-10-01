62011ac1d2099098b75c946295dec9b6d4df3e88
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2023-10-01 21:18:56 +0530

    Time: 7 ms (73.24%), Space: 6.4 MB (10.23%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,15 +3,11 @@ public:
     bool isPalindrome(int x) {
         string s = to_string(x);
         int j = s.length()-1;
-        int i=0,flag=0;
-        while(i<j){
-            if(s[i]==s[j])
-                flag=1;
-            else{
-                flag=0;
+        int i=0;
+        while(i<=j){
+            if(s[i]!=s[j])
                 return false;
-                break;
-            }
+            
             i++;
             j--;
         }
